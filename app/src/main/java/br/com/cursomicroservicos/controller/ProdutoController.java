a6548af6ec8563a4fd11455ce468d14a5b3d703c
Muehlner <muehlner.arao@gmail.com>
2023-06-11 04:15:28 -0300

    adicionando banco de dados

--- app/src/main/java/br/com/cursomicroservicos/controller/ProdutoController.java
+++ app/src/main/java/br/com/cursomicroservicos/controller/ProdutoController.java
@@ -5,13 +5,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cursomicroservicos.model.Produto;
+import br.com.cursomicroservicos.model.ProdutoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
-@RequestMapping("produto")
+@RequestMapping("/produto")
+@AllArgsConstructor
 public class ProdutoController {
 
-    @GetMapping
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("FUNCIONANDO");
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Produto>> get() {
+
+        List<Produto> produtos = produtoRepository.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(produtos);
     }
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Produto> post(@RequestBody Produto produto) {
+
+        Produto retornoDb = produtoRepository.save(produto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(retornoDb);
+    }
 }
